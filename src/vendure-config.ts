5f8c34045a5317169ab393a94d2693f21c5bf6f4
Imtiyaz94 <imtiyaz@microcosmworks.com>
2023-03-15 12:41:45 +0530

    deployment

---
+++ src/vendure-config.ts
@@ -0,0 +1,121 @@
+import {
+  dummyPaymentHandler,
+  DefaultJobQueuePlugin,
+  DefaultSearchPlugin,
+  VendureConfig,
+  DefaultOrderPlacedStrategy,
+} from '@vendure/core';
+import { defaultEmailHandlers, EmailPlugin } from '@vendure/email-plugin';
+import { AssetServerPlugin } from '@vendure/asset-server-plugin';
+import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
+import { StripePlugin } from '@vendure/payments-plugin/package/stripe';
+import 'dotenv/config';
+import path from 'path';
+
+const IS_DEV = process.env.APP_ENV === 'dev';
+
+export const config: VendureConfig = {
+  apiOptions: {
+    port: 4000,
+    adminApiPath: 'admin-api',
+    shopApiPath: 'shop-api',
+    // The following options are useful in development mode,
+    // but are best turned off for production for security
+    // reasons.
+    ...(IS_DEV
+      ? {
+          adminApiPlayground: {
+            settings: { 'request.credentials': 'include' } as any,
+          },
+          adminApiDebug: true,
+          shopApiPlayground: {
+            settings: { 'request.credentials': 'include' } as any,
+          },
+          shopApiDebug: true,
+        }
+      : {}),
+  },
+  authOptions: {
+    tokenMethod: ['bearer', 'cookie'],
+    superadminCredentials: {
+      identifier: process.env.SUPERADMIN_USERNAME,
+      password: process.env.SUPERADMIN_PASSWORD,
+    },
+    cookieOptions: {
+      secret: process.env.COOKIE_SECRET,
+    },
+  },
+  dbConnectionOptions: {
+    type: 'mysql',
+    // See the README.md "Migrations" section for an explanation of
+    // the `synchronize` and `migrations` options.
+    synchronize: false,
+    migrations: [path.join(__dirname, './migrations/*.+(js|ts)')],
+    logging: false,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  },
+  paymentOptions: {
+    paymentMethodHandlers: [dummyPaymentHandler],
+  },
+  // When adding or altering custom field definitions, the database will
+  // need to be updated. See the "Migrations" section in README.md.
+  customFields: {},
+  // orderOptions: {
+  //   process: [DefaultOrderPlacedStrategy],
+  // },
+  plugins: [
+    StripePlugin.init({
+      apiKey:
+        'sk_test_51Mbe6PSBzNgGxdJjW1UI7PPQ09ZC1OEY5iM1rpe0XSy0wqqwCkw0eg4BIOjENUGnoQtuI8TsU91DNxes3kVDU96R00vNICsYmq',
+      webhookSigningSecret:
+        'whsec_e4a24049367d91b8541ad6b4d1e6572c29ed19bf7f9d0e5a1d3ace7009578bc9',
+      // This prevents different customers from using the same PaymentIntent
+      storeCustomersInStripe: false,
+    }),
+
+    AssetServerPlugin.init({
+      route: 'assets',
+      assetUploadDir: path.join(__dirname, '../static/assets'),
+      // For local dev, the correct value for assetUrlPrefix should
+      // be guessed correctly, but for production it will usually need
+      // to be set manually to match your production url.
+      assetUrlPrefix: IS_DEV ? undefined : 'https://www.my-shop.com/assets',
+    }),
+    DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
+    DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
+    EmailPlugin.init({
+      // devMode: false,
+      // outputPath: path.join(__dirname, '../static/email/test-emails'),
+      // route: 'mailbox',
+      handlers: defaultEmailHandlers,
+      templatePath: path.join(__dirname, '../static/email/templates'),
+      globalTemplateVars: {
+        // The following variables will change depending on your storefront implementation.
+        // Here we are assuming a storefront running at http://localhost:8080.
+        fromAddress: 'imtiyaz@microcosmworks.com',
+        verifyEmailAddressUrl: 'http://localhost:3000/verify',
+        passwordResetUrl: 'http://localhost:3000/password-reset',
+        changeEmailAddressUrl:
+          'http://localhost:3000/verify-email-address-change',
+      },
+      transport: {
+        type: 'smtp',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'imtiyazmicro81@gmail.com',
+          pass: 'npsogfcoqrznoged',
+        },
+      },
+    }),
+    AdminUiPlugin.init({
+      route: 'admin',
+      port: 3002,
+    }),
+  ],
+};
